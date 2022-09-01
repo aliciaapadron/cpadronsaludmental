dcd9b0360b88885ca0dfd35e8696f4a29557c45e
Alicia Padrón <andy+pruebaalicia@lin3s.com>
2022-09-01 12:33:28 +0200

    add text in aboutme

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -10,25 +10,178 @@ export function AboutMe() {
         <h2 className="aboutsection__title">
           Conóceme <br />
         </h2>
-
         <img
           className="aboutsection__img"
           src={eli}
           alt="Imagen de Cris y Eli"
           title="Imagen de Cris y Eli"
-        />
-        <p className="aboutsection__text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          hic eos, soluta dolorem, ea explicabo incidunt in quod ratione
-          facilis, corporis expedita odit exercitationem dolorum rem. Suscipit
-          similique ad incidunt! Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Vel enim consectetur sint corrupti quod, molestiae
-          facere dignissimos nam repudiandae aspernatur optio facilis
-          voluptatem, libero exercitationem tempore at eius. Numquam, corrupti!
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus,
-          minus deleniti inventore unde maxime ab tempore, asperiores, aliquam
-          magnam laudantium dolores fugit incidunt. Perspiciatis recusandae
-          deleniti maiores, sapiente doloremque ex.
+        />{' '}
+      </section>
+      <section className="aboutmesection">
+        <p className="aboutmesection__text">
+          Soy Cris, mujer Autista e Hiperactiva. Soy{' '}
+          <span className="bold">Experta en Autismo</span> y{' '}
+          <span className="bold">Experta en (T)DAH</span>. Además de{' '}
+          <span className="bold">Educadora/Mediadora Afectivo-sexual</span> y
+          Educadora Familiar.
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          Acompaño a personas Autistas y/o (T)TDAH a{' '}
+          <span className="bold">conocer su identidad</span> detrás del{' '}
+          <span className="bold">masking</span>, a respetar sus{' '}
+          <span className="bold">necesidades sensoriales</span>, a{' '}
+          <span className="bold">practicar la compasión</span> con si
+          mismos/as/es y a <spn className="bold">relacionarse exitosamente</spn>{' '}
+          con sus seres queridos.
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          Mi objetivo es ofrecer un servicio{' '}
+          <span className="bold">
+            sin jerarquizar, de Neurodivergente a Neurodivergente.
+          </span>
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          Estoy formada en{' '}
+          <span className="bold">
+            Altas Capacidades, Doble Excepcionalidad, Trastornos de la Conducta
+            Alimentaria
+          </span>{' '}
+          y otras <span className="bold">co-ocurrencias</span> con el Autismo y
+          el (T)DAH.
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          Durante mis <span className="bold">años de experiencia</span> he
+          acompañado a <span className="bold">personas Autistas y (T)DAH</span>{' '}
+          a{' '}
+          <span className="bold">
+            encontrarse a si mismos/as/es en la adultez, comprenderse,
+            respetarse y priorizar sus necesidades
+          </span>
+          .
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          Por otro lado, he acompañado a <span className="bold">menores</span> a
+          transitar el{' '}
+          <span className="bold">
+            sistema educativo (técnicas de memorización, organización…)
+          </span>{' '}
+          , la vida social y familiar. A sus ma/padres para{' '}
+          <span className="bold">abordar su identidad</span>y las{' '}
+          <span className="bold">dificultades derivadas</span>. También a
+          <span className="bold"> ma/padres a descubrir su Autismo/(T)DAH</span>{' '}
+          a partir del de sus hijos/as/es.
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          Acompaño desde un enfoque
+          <span className="bold"> LGTB+ friendly</span>, enfoque{' '}
+          <span className="bold">de género</span>,{' '}
+          <span className="bold">antigordófobo</span>, alida{' '}
+          <span className="bold">antirracista</span> y{' '}
+          <span className="bold">respetuosa</span> con las{' '}
+          <span className="bold">espiritualidades</span>.
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          En mi equipo cuento con el trabajo de Patricia,{' '}
+          <span className="bold">Neuropsicóloga</span>, para realizar los
+          diagnósticos.
+        </p>
+        <br />
+        <br />
+        <p className="aboutmesection__text italic underline">¿Más personal?</p>
+        <br />
+        <p className="aboutmesection__text">
+          Conseguir mi diagnóstico de Autismo y (T)DAH a los 26 años me hizo
+          repasar el trauma que conlleva la falta de diagnóstico temprano. Lo
+          que me impulsó a ofrecerle a otros/as/es el{' '}
+          <span className="bold">Soporte y el Acompañamiento</span> que yo
+          hubiera deseado.
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          A los 16 años comencé mi{' '}
+          <span className="bold">
+            odisea por los Servicios de Salud Mental{' '}
+          </span>
+          públicos: sobremedicalización, mala praxis, atención deficiente{' '}
+          <span className="bold">
+            sobremedicalización, mala praxis, atención deficiente
+          </span>
+          … No me fue mejor por lo privado. En total pasé por{' '}
+          <span className="bold">2 psiquitras y 5 psicos</span>, pero lo que{' '}
+          <span className="bold">rompió la Brecha de Acceso</span> a mi{' '}
+          <span className="bold">diagnóstico</span> fueron las{' '}
+          <span className="bold">Redes Sociales</span>. Trasteando sobre la{' '}
+          <span className="bold">Alta Sensibilidad</span> (High Sensitivity)
+          llegué a la <span className="bold">Hiper Sensibilidad Autista</span>.
+          Ahí me dejé llevar por Hiperfocos de{' '}
+          <span className="bold">información</span> sobre{' '}
+          <span className="bold">otras mujeres y personas No Binarias</span>{' '}
+          (socializadas como mujeres) <span className="bold">autistas</span>, en
+          cuyas características <span className="bold">me vi reflejada</span>.
+          Lo que me llevó al <span className="bold">diagnóstico oficial</span>.
+        </p>
+        <br />
+        <p className="aboutmesection__text">
+          <span className="bold">Encontrarme como Mujer Autista</span> le dio{' '}
+          <span className="bold">sentido a mis vivencias</span> y me ayudó a
+          <span className="bold">comprenderme</span>. Me ha impulsado a{' '}
+          <span className="bold">dejar el masking</span>, ser{' '}
+          <span className="bold">compasiva</span> conmigo misma y{' '}
+          <span className="bold">honrar mis necesidades</span>. Así como a{' '}
+          <span className="bold">identificar</span> los{' '}
+          <span className="bold">precipitantes</span> de mis colapsos
+          <span className="bold">sensoriales</span>, a entender mi{' '}
+          <span className="bold">maternidad</span> y mis{' '}
+          <span className="bold">relaciones sociales</span>.
+        </p>
+      </section>
+      <br />
+      <section className="aboutmelist">
+        <p className="aboutmelist__text underline">Servicios</p>
+        <br />
+        <ul className="aboutmelist__list">
+          <li className="aboutmelist__list--item">
+            <span className="bold">Acompañamiento Emocional</span>
+          </li>
+          <li className="aboutmelist__list--item">
+            <span className="bold">Autoconocimiento</span> como Autista/(T)DAH
+          </li>
+          <li className="aboutmelist__list--item">
+            <span className="bold">Prevención de crisis</span> (Meltdown y
+            Shutdown)
+          </li>
+          <li className="aboutmelist__list--item">
+            <span className="bold">Derivación</span> para el{' '}
+            <span className="bold">diagnóstico</span>
+          </li>
+          <li className="aboutmelist__list--item">
+            Mediación y Ed. <span className="bold">Afectivo-Sexual</span>
+          </li>
+          <li className="aboutmelist__list--item">
+            Ed. <span className="bold">Familiar</span>
+          </li>
+          <li className="aboutmelist__list--item">
+            <span className="bold">Alimentación</span> y (T)EA/(T)DAH
+          </li>
+          <li className="aboutmelist__list--item">
+            <span className="bold">Maternidad</span>
+          </li>
+          <li className="aboutmelist__list--item">
+            <span className="bold">Consciencia plena </span> y{' '}
+            <span className="bold">compasión (mindfulness)</span>
+          </li>
+        </ul>
+        <br />
+        <p>
+          De estos servicios pueden beneficiarse tanto adultos/as/es como
+          menores.
         </p>
       </section>
       <Footer />
